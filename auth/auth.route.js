b0167c47120c5fb416a014f5609977011995e447
Ezequiel Ercegovich <ercegovich.ezequiel@gmail.com>
2024-07-10 17:22:26 -0300

    eze

--- auth/auth.route.js
+++ auth/auth.route.js
@@ -6,8 +6,8 @@ const authRouter = express.Router();
 
 
 
-authRouter.post('/login', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+authRouter.post('', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173/login');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
