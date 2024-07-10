a4f7ae23c3fbc976a151e593a3636253394c283a
Ezequiel Ercegovich <ercegovich.ezequiel@gmail.com>
2024-07-10 17:12:52 -0300

    eze

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ const corsMiddleware = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
     if (req.method === 'OPTIONS') {
         return res.sendStatus(200);
     }
