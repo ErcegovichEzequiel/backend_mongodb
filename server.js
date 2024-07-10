a0d1c219a7eedd053284cf9f6e27828a135f45e9
Ezequiel Ercegovich <ercegovich.ezequiel@gmail.com>
2024-07-10 15:30:49 -0300

    eze

--- server.js
+++ server.js
@@ -12,12 +12,25 @@ const { cartsRouter } = require("./carts/carts.router");
 const PORT = process.env.PORT || 4000;
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+const corsMiddleware = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+};
+
+app.use(corsMiddleware);
+
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+//     allowedHeaders: ['Content-Type', 'Authorization']
+// }));
 
 app.use(express.json());
 
