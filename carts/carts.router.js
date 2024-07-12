54a868fdfa3555a6792adb49c28e42fa678957f5
Ezequiel Ercegovich <ercegovich.ezequiel@gmail.com>
2024-07-12 17:51:22 -0300

    eze

--- carts/carts.router.js
+++ carts/carts.router.js
@@ -1,17 +1,14 @@
 const express = require('express')
 
-// const { verifyTokenMiddlewar } = require('../auth/auth.middleware')
+const { verifyTokenMiddlewar } = require('../auth/auth.middleware')
 const { postCartController, getCartController, deleteProductFromCartController } = require('./carts.controller')
 
 const cartsRouter = express.Router()
 
-// cartsRouter.get('/', verifyTokenMiddlewar, getCartController)
-// cartsRouter.post('/', verifyTokenMiddlewar, postCartController)
-// cartsRouter.delete('/:product_id', verifyTokenMiddlewar, deleteProductFromCartController)
+cartsRouter.get('/', verifyTokenMiddlewar, getCartController)
+cartsRouter.post('/', verifyTokenMiddlewar, postCartController)
+cartsRouter.delete('/:product_id', verifyTokenMiddlewar, deleteProductFromCartController)
 
-cartsRouter.get('/',  getCartController)
-cartsRouter.post('/',  postCartController)
-cartsRouter.delete('/:product_id',  deleteProductFromCartController)
 
 
 
