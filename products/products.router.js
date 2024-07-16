2cbe0378f07e72aa9a1860911ffe67a927fd2cc0
Ezequiel Ercegovich <ercegovich.ezequiel@gmail.com>
2024-07-16 00:21:44 -0300

    first commit

--- products/products.router.js
+++ products/products.router.js
@@ -3,7 +3,7 @@ const { verifyTokenMiddlewar } = require('../auth/auth.middleware')
 const { createProductController, getProductByIdController, deleteProductById, putProduct, getAllProducts } = require('./products.controller')
 const productRouter = express.Router()
 
-productRouter.get('/', verifyTokenMiddlewar, getAllProducts)
+productRouter.get('/', getAllProducts)
 productRouter.post('/createProduct', verifyTokenMiddlewar, createProductController) //    Crear un nuevo producto 
 productRouter.put('/:pid', verifyTokenMiddlewar, putProduct) // actualizar un producto por su id
 productRouter.delete('/:pid', verifyTokenMiddlewar, deleteProductById) // borrar un producto por su id
